19623eabd5206181c95b983a9ab6aa7d5f03d026
ali <ali.akeelqq123@gmail.com>
2024-08-18 11:10:37 +0300

    added form_letter template

---
+++ lib/event_manager.rb
@@ -0,0 +1,45 @@
+require 'csv'
+require 'google/apis/civicinfo_v2'
+
+puts 'event manager initalized!'
+
+def clean_zipcode(zipcode)
+  zipcode.to_s.rjust(5, '0')[0..4]
+end
+
+def legislator_by_zipcode(zip)
+  civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
+  civic_info.key = 'AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw'
+
+  begin
+    legislators = civic_info.representative_info_by_address(
+      address: zip,
+      levels: 'country',
+      roles: ['legislatorUpperBody', 'legislatorLowerBody']
+    )
+    legislators = legislators.officials
+
+    legislator_names = legislators.map do |legislator|
+      legislator.name
+    end
+
+    legislator_names.join(", ")
+
+    rescue
+      'you can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials'
+    end
+end
+
+
+contents = CSV.open('event_attendees.csv',
+headers: true,
+header_converters: :symbol)
+
+contents.each do |row|
+  name = row[:first_name]
+
+  zipcode = clean_zipcode(row[:zipcode])
+
+  legislators = legislator_by_zipcode(zipcode)
+  puts "#{name} #{zipcode} #{legislators}"
+end
