27343baa48a5bbeb97f6de1e18b40b2abb2254de
ali <ali.akeelqq123@gmail.com>
2024-08-27 11:15:04 +0300

    clean up clean_phone_number code

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -33,8 +33,29 @@ def save_thank_you_letter(id,form_letter)
 
 end
 
-def phone_number(number)
+def clean_phone_number(number)
+  arr_number = number.to_s.chars
+  unwanted_characters = ["-", "(", ")", " ", "."]
+  arr_number.reject! do |element|
+    unwanted_characters.include?(element)
+  end
+   case arr_number.length
 
+   when 0..9
+    arr_number = "phone number unavailable"
+   when 11
+    if arr_number[0] == '1'
+      arr_number.shift
+    else
+      arr_number = "phone number unavailable"
+    end
+  when 10
+
+  else
+    arr_number = "phone number unavailable"
+  end
+  arr_number = arr_number.join if arr_number.is_a?(Array)
+  return arr_number
 end
 
 
@@ -49,34 +70,16 @@ erb_template = ERB.new(template_letter)
 contents.each do |row|
   id = row[0]
   name = row[:first_name]
-   phone_number = row[5]
-   arr_number = phone_number.to_s.chars
-   unwanted_characters = ["-", "(", ")", " ", "."]
-   arr_number.reject! do |element|
-    unwanted_characters.include?(element)
-   end
-   puts "Original phone number: #{phone_number}, Array: #{arr_number}"
-   case arr_number.length
+  phone_number = row[:homephone]
 
-   when 0..9
-    arr_number = "phone number unavailable"
-   when 11
-    if arr_number[0] == '1'
-      arr_number.shift
-      p arr_number
-    else
-      arr_number = "phone number unavailable"
-    end
-  when 10
-
-  else
-    arr_number = "phone number unavailable"
-  end
   zipcode = clean_zipcode(row[:zipcode])
 
   legislators = legislator_by_zipcode(zipcode)
 
   form_letter = erb_template.result(binding)
+
   save_thank_you_letter(id,form_letter)
-  puts "#{arr_number}"
+
+  clean_number = clean_phone_number(phone_number)
+  puts "#{name} #{clean_number}"
 end
